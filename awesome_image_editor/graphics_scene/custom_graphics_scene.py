8c0a29fce74b1c3ee74567a45025082bca7b4956
Iyad Ahmed <iyadahmed430@gmail.com>
2022-10-07 13:18:47 +0200

    Fix #27: Draw selected items bounding box overlay

--- awesome_image_editor/graphics_scene/custom_graphics_scene.py
+++ awesome_image_editor/graphics_scene/custom_graphics_scene.py
@@ -1,4 +1,5 @@
-from PyQt6.QtCore import pyqtSignal
+from PyQt6.QtCore import pyqtSignal, QRectF
+from PyQt6.QtGui import QPainter
 from PyQt6.QtWidgets import QGraphicsScene, QGraphicsItem
 
 
@@ -6,7 +7,21 @@ class QGraphicsSceneCustom(QGraphicsScene):
     itemAboutToBeAppended = pyqtSignal()
     itemAppended = pyqtSignal(QGraphicsItem)
 
+    def __init__(self):
+        super().__init__()
+        self.selectionChanged.connect(self.update)
+        self.changed.connect(self.update)
+
     def addItem(self, item: QGraphicsItem) -> None:
         self.itemAboutToBeAppended.emit()
         super().addItem(item)
         self.itemAppended.emit(item)
+
+    def drawForeground(self, painter: QPainter, rect: QRectF) -> None:
+        # Draw selection bounding box
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing, False)
+        rect = QRectF()
+        for item in self.selectedItems():
+            rect = rect.united(item.sceneBoundingRect())
+        painter.drawRect(rect)
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing, True)
