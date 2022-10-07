641a50aeecf696b9b79b4b070d6a957333dcb73a
Iyad Ahmed <iyadahmed430@gmail.com>
2022-10-07 17:00:36 +0200

    Use custom shape item class instead of inheriting from QGraphicsPathItem
    Now that shape layers are selectable, qt by default draws a selection bounding box around them, we do not want that, since the selection bounding box is drawn in the foreground layer of the graphics scene, this commit effectively disables the default  dotted lines selection bounding box for shape layer, we could have done in a different way but this way is less confusing

--- awesome_image_editor/graphics_scene/items/shape.py
+++ awesome_image_editor/graphics_scene/items/shape.py
@@ -1,14 +1,24 @@
-from PyQt6.QtGui import QPainterPath
-from PyQt6.QtWidgets import QGraphicsPathItem, QGraphicsItem
+from typing import Optional
+
+from PyQt6.QtCore import QRectF
+from PyQt6.QtGui import QPainterPath, QPainter
+from PyQt6.QtWidgets import QStyleOptionGraphicsItem, QWidget, QGraphicsItem
 
 
-class AIEShapeItem(QGraphicsPathItem):
+class AIEShapeItem(QGraphicsItem):
     def __init__(self, path: QPainterPath, name: str):
-        super().__init__(path, None)
+        super().__init__()
         self.name = name
+        self.path = path
         self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsSelectable, True)
         self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable, True)
 
     def get_thumbnail(self): ...
 
     def get_size_hint(self): ...
+
+    def boundingRect(self) -> QRectF:
+        return self.path.boundingRect()
+
+    def paint(self, painter: QPainter, option: QStyleOptionGraphicsItem, widget: Optional[QWidget] = ...) -> None:
+        painter.drawPath(self.path)
