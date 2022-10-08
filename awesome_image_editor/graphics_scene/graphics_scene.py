7a2a2e4ea5d447ce8982781a404c87265b6366f3
Iyad Ahmed <iyadahmed430@gmail.com>
2022-10-08 20:58:28 +0200

    Refactor selection bounding box overlay

--- awesome_image_editor/graphics_scene/graphics_scene.py
+++ awesome_image_editor/graphics_scene/graphics_scene.py
@@ -9,13 +9,21 @@ class AIEGraphicsScene(QGraphicsScene):
 
     def __init__(self):
         super().__init__()
-        self.selectionChanged.connect(self.update)
-        self.changed.connect(self.update)
+        self.changed.connect(self._invalidate_foreground)
+
+    def _invalidate_foreground(self):
+        self.invalidate(self.sceneRect(), QGraphicsScene.SceneLayer.ForegroundLayer)
 
     def addItem(self, item: QGraphicsItem) -> None:
         self.itemAboutToBeAppended.emit(len(self.items()))
         super().addItem(item)
         self.itemAppended.emit()
+
+    def _calc_selected_items_bounding_box(self):
+        rect = QRectF()
+        for item in self.selectedItems():
+            rect = rect.united(item.sceneBoundingRect())
+        return rect
 
     def drawForeground(self, painter: QPainter, rect: QRectF) -> None:
         # Draw selection bounding box
@@ -23,8 +31,5 @@ class AIEGraphicsScene(QGraphicsScene):
         painter.setRenderHint(QPainter.RenderHint.Antialiasing, False)
         painter.setCompositionMode(QPainter.CompositionMode.RasterOp_SourceXorDestination)
         painter.setPen(QColor(255, 255, 255))
-        rect = QRectF()
-        for item in self.selectedItems():
-            rect = rect.united(item.sceneBoundingRect())
-        painter.drawRect(rect)
+        painter.drawRect(self._calc_selected_items_bounding_box())
         painter.restore()
