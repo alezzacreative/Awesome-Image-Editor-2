44e21345ff1be108ae44eb0ec711fdc059957b76
Iyad Ahmed <iyadahmed430@gmail.com>
2022-10-08 12:00:06 +0200

    Union type hint has to have multiple types, added QIcon to list of possible types returned by get_thumbnail

--- awesome_image_editor/graphics_scene/tree_item.py
+++ awesome_image_editor/graphics_scene/tree_item.py
@@ -1,13 +1,13 @@
 from typing import List, Protocol, Union
 
 from PyQt6.QtCore import QSize
-from PyQt6.QtGui import QImage
+from PyQt6.QtGui import QImage, QIcon
 
 
 class TreeItemProtocol(Protocol):
     name: str
 
-    def get_thumbnail(self) -> Union[QImage]: ...
+    def get_thumbnail(self) -> Union[QImage, QIcon]: ...
 
     def get_size_hint(self) -> QSize: ...
 
