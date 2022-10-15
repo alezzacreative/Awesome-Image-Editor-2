80687b045633ef4a5df33ecbe31cc5dbc90c1b20
Iyad Ahmed <iyadahmed430@gmail.com>
2022-10-15 13:29:47 +0200

    Support PSD text layer

---
+++ awesome_image_editor/graphics_scene/items/type.py
@@ -0,0 +1,43 @@
+from PyQt6.QtCore import Qt, QEvent
+from PyQt6.QtWidgets import QGraphicsTextItem, QGraphicsItem, QStyleOptionGraphicsItem, QWidget, QStyle, \
+    QGraphicsSceneMouseEvent
+from PyQt6.QtGui import QPainter, QPainterPath, QFocusEvent, QTextCursor
+
+
+class AIETextItem(QGraphicsTextItem):
+    def __init__(self, text: str, name: str):
+        super().__init__(text)
+        self.name = name
+        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsSelectable, True)
+        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable, True)
+        self.setTextInteractionFlags(Qt.TextInteractionFlag.NoTextInteraction)
+
+    def paint(self, painter: QPainter, option: QStyleOptionGraphicsItem, widget: QWidget) -> None:
+        option.state &= ~QStyle.StateFlag.State_Enabled
+        option.state &= ~QStyle.StateFlag.State_HasFocus
+        option.state &= ~QStyle.StateFlag.State_Selected
+        super().paint(painter, option, widget)
+
+    def mousePressEvent(self, event: QGraphicsSceneMouseEvent) -> None:
+        super().mousePressEvent(event)
+
+    def mouseDoubleClickEvent(self, event: QGraphicsSceneMouseEvent) -> None:
+        super().mouseDoubleClickEvent(event)
+        self.setTextInteractionFlags(Qt.TextInteractionFlag.TextEditorInteraction)
+
+    def focusOutEvent(self, event: QFocusEvent) -> None:
+        super().focusOutEvent(event)
+        # TODO: focus out text item using ESC and dedicated button instead
+        self.setTextInteractionFlags(Qt.TextInteractionFlag.NoTextInteraction)
+
+        # Clear cursor when focusing out, otherwise selection will remain.
+        # P.S.: We have to create and set a cursor
+        # self.getTextCursor won't allow us to modify the selection, it returns a copy
+        # https://www.qtcentre.org/threads/4065-QGraphicsTextItem-is-it-a-bug-there
+        cursor = QTextCursor()
+        self.setTextCursor(cursor)
+        cursor.clearSelection()
+
+    def get_thumbnail(self): ...
+
+    def get_size_hint(self): ...
--- awesome_image_editor/psd_read.py
+++ awesome_image_editor/psd_read.py
@@ -1,9 +1,13 @@
-from PyQt6.QtGui import QPainterPath
+from PyQt6.QtGui import QPainterPath, QTextCharFormat, QTextCursor, QFont, QColor, qRgba
+from PyQt6.QtWidgets import QGraphicsTextItem
 from psd_tools import PSDImage
 
 from .file_format import AIEProject
 from .graphics_scene.items.image import AIEImageItem
 from .graphics_scene.items.shape import AIEShapeItem
+from .graphics_scene.items.type import AIETextItem
+
+DEFAULT_PSD_TEXT_FILL_COLOR_DATA = {'Type': 1, 'Values': [1, 0, 0, 0]}
 
 
 def add_pixel_layer(scene, layer):
@@ -62,6 +66,46 @@ def add_shape_layer(scene, layer, psd_width, psd_height):
         scene.addItem(item)
 
 
+def add_type_layer(scene, layer):
+    item = AIETextItem("", layer.name)
+    item.setPos(layer.offset[0], layer.offset[1])
+    item.setVisible(layer.visible)
+    scene.addItem(item)
+
+    cursor = QTextCursor(item.document())
+
+    # Extract font for each substring in the text.
+    text = layer.engine_dict['Editor']['Text'].value
+    fontset = layer.resource_dict['FontSet']
+    runlength = layer.engine_dict['StyleRun']['RunLengthArray']
+    rundata = layer.engine_dict['StyleRun']['RunArray']
+    index = 0
+    for length, style in zip(runlength, rundata):
+        substring: str = text[index:index + length]
+
+        stylesheet = style['StyleSheet']['StyleSheetData']
+        font = fontset[stylesheet['Font']]
+        index += length
+
+        fill_color_data = stylesheet.get('FillColor', DEFAULT_PSD_TEXT_FILL_COLOR_DATA)
+        fill_color_argb_float = fill_color_data['Values']
+        fill_color_rgba_float = (*fill_color_argb_float[1:], fill_color_argb_float[0])
+        fill_color_rgba_uchar = tuple(map(lambda x: int(x * 255), fill_color_rgba_float))
+
+        font_family = str(font['Name'])
+        font_size = stylesheet['FontSize']
+
+        qfont = QFont()
+        qfont.setPixelSize(font_size)
+        qfont.setFamily(font_family)
+
+        char_format = QTextCharFormat()
+        char_format.setFont(qfont)
+        char_format.setForeground(QColor(*fill_color_rgba_uchar))
+
+        cursor.insertText(substring, char_format)
+
+
 def load_psd_as_project(filepath):
     psd = PSDImage.open(filepath)
 
@@ -73,5 +117,8 @@ def load_psd_as_project(filepath):
 
         elif layer.kind == "shape":
             add_shape_layer(scene, layer, psd.width, psd.height)
+
+        elif layer.kind == "type":
+            add_type_layer(scene, layer)
 
     return project
